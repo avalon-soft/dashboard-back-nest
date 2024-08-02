ca333f434bd83a54307e2b86a0c2fb0671d7f048
judy.alvarez <a.zhoglikova@disoft.dev>
2024-08-02 10:25:36 +0200

    add table faker

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { faker } from '@faker-js/faker';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getTableData(): any[] {
+    const data = [];
+    for (let i = 0; i < 100; i++) {
+      data.push({
+        id: i + 1,
+        name: faker.internet.userName(),
+        status: faker.helpers.arrayElement(['created', 'in progress', 'success', 'error']),
+      });
+    }
+    return data;
   }
 }
