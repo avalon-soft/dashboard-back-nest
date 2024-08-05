a9e5b6141bb49f6a8d061c92e54c3d5115fc9eca
judy.alvarez <a.zhoglikova@disoft.dev>
2024-08-05 14:58:28 +0200

    fixed

---
+++ src/exceptions.filter.ts
@@ -0,0 +1,25 @@
+import {ArgumentsHost, Catch, ExceptionFilter, HttpException} from '@nestjs/common'
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    let status = 500;
+    let message = 'Internal Server Error';
+
+    if (exception instanceof HttpException) {
+      status = exception.getStatus();
+      message = exception.message;
+    }
+
+    response.status(status).json({
+      statusCode: status,
+      message,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
