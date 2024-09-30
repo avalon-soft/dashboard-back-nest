433441e205ad2656a9757469b856b9e2058a9016
judy.alvarez <a.zhoglikova@disoft.dev>
2024-09-30 14:13:22 +0200

    update user test

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,20 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import {User} from './entities/user.entity';
+import {UserModule} from './user.module'
+import {Repository} from 'typeorm'
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
+
+  const UserRepository = Repository<User>
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
+      // controllers: [UserController],
       providers: [UserService],
+      imports: [UserModule, TypeOrmModule.forFeature([User])],
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    // controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    const createUserDto = {
+      full_name: 'test',
+      email: 'test@gmail.com',
+      password: 'test',
+    };
+    const result = service.create(createUserDto);
+    expect(result).toBeDefined();
+    // expect(result.full_name).toBe(createUserDto.full_name);
+    // expect(result.email).toBe(createUserDto.email);
+    // expect(result.password).toBe(createUserDto.password);
   });
 });
